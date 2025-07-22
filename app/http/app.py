16c46e91c68ba49e2c3eb2e916f18d64e62a544a
LiXiaoYaoCareFree <2210127151@qq.com>
2025-07-22 16:26:29 +0800

    chat Robot API interface

--- app/http/app.py
+++ app/http/app.py
@@ -1,10 +1,13 @@
+import dotenv
 from injector import Injector
 
 from internal.server.http import Http
 from internal.router import Router
 
-
+# 将env加载到环境变量中
+dotenv.load_dotenv()
 injector = Injector()
+
 app = Http(__name__, router=injector.get(Router))
 
 
--- internal/handler/app_handler.py
+++ internal/handler/app_handler.py
@@ -1,5 +1,28 @@
+import os
+
+from flask import request
+from openai import OpenAI
+
 class AppHandler:
     """应用控制器"""
+    def completion(self):
+        """聊天接口"""
+        # 1.提取从接口中获取的输入，GET?POST?
+        query = request.json.get("query")
+
+        # 2.构建Openai客户端，并发起请求
+        client = OpenAI(base_url=os.getenv("OPENAI_API_BASE"))
+
+        # 3.得到请求响应，然后将OpenAI的响应传递给前端
+        completion = client.chat.completions.create(
+            model="kimi-k2-0711-preview",
+            messages=[{"role": "system", "content": "你是一个AI助手，根据用户的输入回答相应的问题"},
+                      {"role":"user", "content": query},
+            ]
+        )
+
+        return completion.choices[0].message.content
 
     def ping(self):
         return {"ping": "pong"}
+
